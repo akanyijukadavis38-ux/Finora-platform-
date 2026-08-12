943b34bcd3447369224be3f6d1a45834defcf334
akanyijukadavis38-ux <akanyijukadavis38@gmail.com>
2026-08-12 20:00:23 +0300

    Update server.js

--- server.js
+++ server.js
@@ -2,13 +2,18 @@ require("dotenv").config();
 
 const express = require("express");
 const cors = require("cors");
+const path = require("path");
 const pool = require("./database");
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use(express.static(path.join(__dirname, "public")));
 
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "index.html"));
+});
 app.get("/", (req, res) => {
   res.json({
     success: true,
