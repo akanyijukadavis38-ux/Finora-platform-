e1ce1ad773e11ec30c8f522ef941e930726de0fc
akanyijukadavis38-ux <akanyijukadavis38@gmail.com>
2026-08-28 04:00:28 +0300

    Update login.js

--- login.js
+++ login.js
@@ -266,7 +266,7 @@ document.addEventListener("DOMContentLoaded", function () {
                 const response =
                     await fetch(
                         API_URL +
-                        "/api/login",
+                        "/api/Users/login",
                         {
 
                             method:
