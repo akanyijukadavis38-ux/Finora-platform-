b1960ad82078771da5e7b5219e790e4bc0ec2d63
akanyijukadavis38-ux <akanyijukadavis38@gmail.com>
2026-08-31 11:54:45 +0300

    Update server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const { connectDB, mongoose } = require("./database");
 const userRoutes = require("./userRoutes");
 
 const app = express();
-
+app.set("trust proxy", 1);
 const PORT = process.env.PORT || 8080;
 
 
